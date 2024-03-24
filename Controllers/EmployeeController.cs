3a0e64f4a45fa4bfc736e86dbaa00833c60a1591
Hüseyin Kurt <kurth2517@gmail.com>
2024-03-24 06:05:06 +0300

    "bitti"

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,20 @@
+﻿ using Microsoft.AspNetCore.Mvc;
+
+namespace Basics.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult  Index1()
+        {
+            return view();
+        }
+        public ViewResult Index2()
+        {
+            return View("Index");
+        }
+        public ActionResult Index3()
+        {
+            return Content("Employee");
+        }
+    }
+}
--- obj/Debug/net8.0/Basics.AssemblyInfo.cs
+++ obj/Debug/net8.0/Basics.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Basics")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f35a778a470cd9d4755d5f9333a27c923b1a2988")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e681bf5d2e77e1fbc9503e219baf13132e25fc25")]
 [assembly: System.Reflection.AssemblyProductAttribute("Basics")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Basics")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
